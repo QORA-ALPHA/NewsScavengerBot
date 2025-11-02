4ea2cec28718279360680f7b49b451da3a60db50
QORA-ALPHA <emjeii.juniior1715@gmail.com>
2025-11-02 00:22:48 -0400

    Delete app directory

--- app/formatting.py
+++
@@ -1,7 +0,0 @@
-import html
-
-def format_item_html(item: dict) -> str:
-    title = html.escape(item.get("title") or "")
-    link = item.get("link") or ""
-    src  = html.escape(item.get("source") or "")
-    return f"<b>{title}</b>\n<i>{src}</i>\n{link}"
